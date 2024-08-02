525cd8ef27aca54c6437eea1ab6e65e0512ee34b
Manisha Goyal <manisha.goyal1996@gmail.com>
2024-08-02 19:34:35 +0000

    Added swagger configuration

--- service/__init__.py
+++ service/__init__.py
@@ -20,8 +20,12 @@ and SQL database
 """
 import sys
 from flask import Flask
+from flask_restx import Api
 from service import config
 from service.common import log_handlers
+
+# Will be initialize when app is created
+api = None  # pylint: disable=invalid-name
 
 
 ############################################################
@@ -37,6 +41,24 @@ def create_app():
     # pylint: disable=import-outside-toplevel
     from service.models import db
     db.init_app(app)
+
+    # Turn off strict slashes because it violates best practices
+    app.url_map.strict_slashes = False
+
+    ######################################################################
+    # Configure Swagger before initializing it
+    ######################################################################
+    global api
+    api = Api(
+        app,
+        version="1.0.0",
+        title="Pet Demo REST API Service",
+        description="This is a sample server Pet store server.",
+        default="pets",
+        default_label="Pet shop operations",
+        doc="/apidocs",  # default also could use doc='/apidocs/'
+        prefix="/api",
+    )
 
     with app.app_context():
         # Dependencies require we import the routes AFTER the Flask app is created
