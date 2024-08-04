11ee94c915d701b5cb5757880cfe9e5e737d804b
Kristo <kp1404@nyu.edu>
2024-08-04 14:05:47 +0000

    Update on add swagger data model

--- service/routes.py
+++ service/routes.py
@@ -22,7 +22,7 @@ and Delete Shopcarts and Shopcart Items
 """
 
 from flask import current_app as app  # Import Flask application
-from flask_restx import Resource, reqparse
+from flask_restx import Resource, reqparse, fields
 from service.models import Shopcart, ShopcartItem
 from service.common import status  # HTTP Status Codes
 from . import api
@@ -46,6 +46,61 @@ def health():
     return {"status": "OK"}, status.HTTP_200_OK
 
 
+# Define the models so that the docs reflect what can be sent
+create_shopcartItem_model = api.model(
+    "ShopcartItem",
+    {
+        "shopcart_id": fields.Integer(required=True, description="ID of the shopcart"),
+        "name": fields.String(required=True, description="Name of the shopcart item"),
+        "product_id": fields.Integer(required=True, description="ID of the product"),
+        "quantity": fields.Integer(
+            required=True, description="Quantity of shopcart item"
+        ),
+        "price": fields.Float(required=True, description="Price of the product"),
+    },
+)
+
+shopcartItem_model = api.inherit(
+    "ShopcartItemModel",
+    create_shopcartItem_model,
+    {
+        "id": fields.Integer(
+            readOnly=True,
+            description="The unique ID of the shopcart item assigned internally by the service",
+        ),
+        "shopcart_id": fields.Integer(
+            readOnly=True,
+            description="The ID of the shopcart to which the shopcart item belongs",
+        ),
+    },
+)
+
+create_shopcart_model = api.model(
+    "Shopcart",
+    {
+        "total_price": fields.Float(
+            required=True, description="Total price of the shopcart"
+        ),
+        "items": fields.List(
+            fields.Nested(shopcartItem_model),
+            required=False,
+            description="Items in the shopcart",
+        ),
+    },
+)
+
+shopcart_model = api.inherit(
+    "ShopcartModel",
+    create_shopcart_model,
+    {
+        "id": fields.Integer(
+            readOnly=True,
+            description="The unique ID of the shopcart assigned internally by the service",
+        ),
+    },
+)
+
+
 # query string arguments
 shopcart_args = reqparse.RequestParser()
 shopcart_args.add_argument(
@@ -53,14 +108,14 @@ shopcart_args.add_argument(
     type=int,
     location="args",
     required=False,
-    help="Product ID of the Items in the Shopcart"
+    help="Product ID of the Items in the Shopcart",
 )
 shopcart_args.add_argument(
     "name",
     type=str,
     location="args",
     required=False,
-    help="Name of the Items in the Shopcart"
+    help="Name of the Items in the Shopcart",
 )
 
 shopcartItem_args = reqparse.RequestParser()
@@ -69,14 +124,10 @@ shopcartItem_args.add_argument(
     type=int,
     location="args",
     required=False,
-    help="Product ID of the Item"
+    help="Product ID of the Item",
 )
 shopcartItem_args.add_argument(
-    "name",
-    type=str,
-    location="args",
-    required=False,
-    help="Name of the Item"
+    "name", type=str, location="args", required=False, help="Name of the Item"
 )
 
 
@@ -104,6 +155,7 @@ class ShopcartResource(Resource):
     # RETRIEVE A SHOPCART
     # ------------------------------------------------------------------
     @api.response(404, "Shopcart not found")
+    @api.marshal_with(shopcart_model)
     def get(self, shopcart_id):
         """
         Retrieve a single Shopcart
@@ -129,6 +181,8 @@ class ShopcartResource(Resource):
     # ------------------------------------------------------------------
     @api.response(404, "Shopcart not found")
     @api.response(400, "The posted Shopcart data was not valid")
+    @api.expect(shopcart_model)
+    @api.marshal_with(shopcart_model)
     def put(self, shopcart_id):
         """
         Update a Shopcart
@@ -188,6 +242,7 @@ class ShopcartCollection(Resource):
     # LIST ALL SHOPCARTS
     # ------------------------------------------------------------------
     @api.expect(shopcart_args, validate=True)
+    @api.marshal_list_with(shopcart_model)
     def get(self):
         """Returns all of the Shopcarts"""
         app.logger.info("Request for Shopcarts list")
@@ -217,6 +272,8 @@ class ShopcartCollection(Resource):
     # CREATE A NEW SHOPCART
     # ------------------------------------------------------------------
     @api.response(400, "The posted Shopcart data was not valid")
+    @api.expect(create_shopcart_model)
+    @api.marshal_with(shopcart_model, code=201)
     def post(self):
         """
         Creates a Shopcart
@@ -234,7 +291,9 @@ class ShopcartCollection(Resource):
         app.logger.info("Shopcart with id [%s] saved!", shopcart.id)
 
         # Return the location of the new item
-        location_url = api.url_for(ShopcartResource, shopcart_id=shopcart.id, _external=True)
+        location_url = api.url_for(
+            ShopcartResource, shopcart_id=shopcart.id, _external=True
+        )
 
         return shopcart.serialize(), status.HTTP_201_CREATED, {"Location": location_url}
 
@@ -266,11 +325,15 @@ class CheckoutResource(Resource):
             )
         shopcart.calculate_total_price()
 
-        app.logger.info("Checked out Shopcart with id [%s], total price is [%s]", shopcart_id, shopcart.total_price)
+        app.logger.info(
+            "Checked out Shopcart with id [%s], total price is [%s]",
+            shopcart_id,
+            shopcart.total_price,
+        )
 
         return {
             "id": shopcart.id,
-            "total_price": float(shopcart.total_price)
+            "total_price": float(shopcart.total_price),
         }, status.HTTP_200_OK
 
 
@@ -294,6 +357,7 @@ class ShopcartItemResource(Resource):
     # RETRIEVE AN ITEM FROM A SHOPCART
     # ------------------------------------------------------------------
     @api.response(404, "Shopcart Item not found")
+    @api.marshal_with(shopcartItem_model)
     def get(self, shopcart_id, item_id):
         """
         Retrieve a single Item from Shopcart
@@ -301,7 +365,9 @@ class ShopcartItemResource(Resource):
         This endpoint will return a Item from Shopcart based on it's id
         """
         app.logger.info(
-            "Request to Retrieve a Item with id [%s] from Shopcart with id [%s]", item_id, shopcart_id,
+            "Request to Retrieve a Item with id [%s] from Shopcart with id [%s]",
+            item_id,
+            shopcart_id,
         )
 
         # Attempt to find the Shopcart and abort if not found
@@ -330,6 +396,8 @@ class ShopcartItemResource(Resource):
     # ------------------------------------------------------------------
     @api.response(404, "Shopcart Item not found")
     @api.response(400, "The posted Shopcart Item data was not valid")
+    @api.expect(shopcartItem_model)
+    @api.marshal_with(shopcartItem_model)
     def put(self, shopcart_id, item_id):
         """
         Update an Item in a Shopcart
@@ -337,7 +405,9 @@ class ShopcartItemResource(Resource):
         This endpoint will update an Item in a Shopcart based on the body that is posted
         """
         app.logger.info(
-            "Request to update Item with id [%s] in Shopcart with id [%s]", item_id, shopcart_id
+            "Request to update Item with id [%s] in Shopcart with id [%s]",
+            item_id,
+            shopcart_id,
         )
 
         # See if the shopcart exists and abort if it doesn't
@@ -384,7 +454,9 @@ class ShopcartItemResource(Resource):
         This endpoint will delete an Item based on the id specified in the path
         """
         app.logger.info(
-            "Request to delete Item with id [%s] in Shopcart with id [%s]", item_id, shopcart_id
+            "Request to delete Item with id [%s] in Shopcart with id [%s]",
+            item_id,
+            shopcart_id,
         )
 
         # See if the shopcart exists and abort if it doesn't
@@ -400,7 +472,11 @@ class ShopcartItemResource(Resource):
         if item:
             item.delete()
             shopcart.calculate_total_price()
-            app.logger.info("Item with id [%s] deleted from Shopcart with id [%s]!", item_id, shopcart_id)
+            app.logger.info(
+                "Item with id [%s] deleted from Shopcart with id [%s]!",
+                item_id,
+                shopcart_id,
+            )
 
         return "", status.HTTP_204_NO_CONTENT
 
@@ -417,6 +493,7 @@ class ShopcartItemCollection(Resource):
     # LIST ALL ITEMS IN A SHOPCART
     # ------------------------------------------------------------------
     @api.expect(shopcartItem_args, validate=True)
+    @api.marshal_list_with(shopcartItem_model)
     def get(self, shopcart_id):
         """
         Retrieve all Items in a Shopcart
@@ -459,6 +536,8 @@ class ShopcartItemCollection(Resource):
     # ADD AN ITEM TO A SHOPCART
     # ------------------------------------------------------------------
     @api.response(400, "The posted Shopcart Item data was not valid")
+    @api.expect(create_shopcartItem_model)
+    @api.marshal_with(shopcartItem_model, code=201)
     def post(self, shopcart_id):
         """
         Add an Item in a Shopcart
@@ -479,7 +558,9 @@ class ShopcartItemCollection(Resource):
 
         app.logger.info("Processing: %s", data)
 
-        item = ShopcartItem.find_by_product_id_shopcart_id(data["product_id"], shopcart_id)
+        item = ShopcartItem.find_by_product_id_shopcart_id(
+            data["product_id"], shopcart_id
+        )
         if item:
             # Update quantity if the item exists in the shopcart
             item.quantity += data["quantity"]
@@ -494,11 +575,16 @@ class ShopcartItemCollection(Resource):
 
         # update the total price of the shopcart
         shopcart.calculate_total_price()
-        app.logger.info("Item with id [%s] saved in Shopcart with id [%s]!", item.id, shopcart_id)
+        app.logger.info(
+            "Item with id [%s] saved in Shopcart with id [%s]!", item.id, shopcart_id
+        )
 
         # Return the location of the new item
         location_url = api.url_for(
-            ShopcartItemResource, item_id=item.id, shopcart_id=shopcart_id, _external=True
+            ShopcartItemResource,
+            item_id=item.id,
+            shopcart_id=shopcart_id,
+            _external=True,
         )
 
         return item.serialize(), status.HTTP_201_CREATED, {"Location": location_url}
@@ -537,6 +623,7 @@ class ShopcartItemCollection(Resource):
 ######################################################################
 #  U T I L I T Y   F U N C T I O N S
 ######################################################################
+
 
 # ------------------------------------------------------------------
 # Logs error messages before aborting
